1e8465af06867a694b45a8fbf8c5362c58af29bf
rokkuci <kuciauskas.rokas@gmail.com>
2014-09-06 17:47:29 +0300

    indexxUpdate

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Learn PHP by commiting.</title>
+</head>
+<body>
 
+<div class="functions">
+	
+	<?php 
 
+		$files = scandir('./functions');
+		sort($files); // this does the sorting
+	?>
 
-?> 
+		<h2><?php echo "Functions <br />"; ?></h2>
+
+		<?php  
+			foreach($files as $file)
+			{
+				if ($file != ".") 
+				{
+					if ($file != "..") 
+					{
+						echo'<a href="'.$file.'">'.$file.'</a>';
+						echo "<br />";				
+					}
+				} 
+			}	
+
+	?>
+</div>
+</body>
+</html>
